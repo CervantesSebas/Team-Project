be9697de18e8290a40256a4411be32c8ef47d5dd
Eleonora <eliya.kg@gmail.com>
2024-10-15 16:05:31 -0500

    Update mylist={.py

--- mylist={.py
+++ mylist={.py
@@ -1,3 +1,5 @@
+
+x = input("choose from the options below: ")
 mylist={
     "1.", "Welcome"
     "2.", "Say Hi to Eleonora"
@@ -5,4 +7,5 @@ mylist={
     "4.", "Say, How are you?"
     "5.", "Pick Apple"
     "0.", "Exit"}
-}
+for list in mylist:
+    print(list + mylist()list)
