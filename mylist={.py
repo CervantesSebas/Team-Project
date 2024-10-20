92253fd7e25c604945dca90047247302b01a384f
Eleonora <eliya.kg@gmail.com>
2024-10-20 00:45:13 -0500

    Update mylist={.py
    not clicking, but with numbers. works!

--- mylist={.py
+++ mylist={.py
@@ -1,11 +1,22 @@
+list = print(  "1. Welcome "
+    "2. Say Hi to Eleonora "
+    "3. Say Hi to Sebastian "
+    "4. Say, How are you? "
+    "5. Pick Apple "
+    "0. Exit ") 
 
-x = input("choose from the options below: ")
-mylist={
-    "1.", "Welcome"
-    "2.", "Say Hi to Eleonora"
-    "3.", "Say Hi to Sebastian"
-    "4.", "Say, How are you?"
-    "5.", "Pick Apple"
-    "0.", "Exit"}
-for list in mylist:
-    print(list + mylist()list)
+choose = input("choose from below: ")
+
+if choose == "1":
+    print("Welcome")
+elif choose == "2":
+    print("Hi. Eleonora")
+elif choose == "3":
+    print("HI. Sebastian")
+elif choose == "4":
+    print("how are you?")
+elif choose == "5":
+    print("Pick Apple")
+elif choose == "0":
+    print("Exit")
+else: print("invalid choice")
